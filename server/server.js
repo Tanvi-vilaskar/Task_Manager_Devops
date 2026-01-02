20df20f6eb14a062329e4d30edf3ef9ba8ea6bf8
Tanvi-Vilaskar <tvilaskar@gmail.com>
2026-01-03 01:13:53 +0530

    resolve cors issues

--- server/server.js
+++ server/server.js
@@ -11,11 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
